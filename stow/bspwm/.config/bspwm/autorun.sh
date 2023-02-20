f86677deff731624cdb6928545e691d60648efb3
Karlo Miličević <karlo.milicevic@5x9networks.com>
2023-02-20 18:18:07 +0100

    Update

--- stow/bspwm/.config/bspwm/autorun.sh
+++ stow/bspwm/.config/bspwm/autorun.sh
@@ -19,6 +19,10 @@ function set_desktop_wallpaper() {
 set_desktop_wallpaper &
 setxkbmap -layout 'us,hr' -option 'grp:win_space_toggle'
 xset r rate 200 50
+xset s expose
+xset s blank
+xset s 120 5
+XSECURELOCK_SAVER="saver_mpv" XSECURELOCK_BLANK_TIMEOUT=-1 XSECURELOCK_BLANK_DPMS_STATE="on" xss-lock -n /usr/lib/xsecurelock/dimmer -l -- xsecurelock &
 
 run_once nm-applet
 run_once flameshot
--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -2,6 +2,9 @@ export EDITOR=nano
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export MANWIDTH=tty
 
+set -e SSH_AGENT_PID
+export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
+
 if status --is-interactive
 	source ~/.config/fish/fish_command_timer.fish
 	bind --user -s \cx "commandline (history | head -n 1); commandline -f execute"
@@ -15,3 +18,5 @@ if status --is-interactive
 		echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
 	end
 end
+
+source /opt/asdf-vm/asdf.fish
